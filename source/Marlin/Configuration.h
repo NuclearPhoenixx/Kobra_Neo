33d111cb133600e64f39f4b90c584a802cf8489e
NuclearPhoenixx <nuclearphoenix@public-files.de>
2023-11-02 19:39:07 +0100

    update comment

--- source/Marlin/Configuration.h
+++ source/Marlin/Configuration.h
@@ -767,7 +767,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 390 }     //原来是390
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 390 }     // E0 is the extruder steps, important for flow! Default values...
 
 /**
  * Default Max Feed Rate (mm/s)
