b22857f11d917dc79b80fd267409d0acb7bf0e64
NuclearPhoenixx <nuclearphoenix@public-files.de>
2023-11-02 18:29:51 +0100

    change FW version

--- source/Marlin/src/lcd/menu/menu.h
+++ source/Marlin/src/lcd/menu/menu.h
@@ -31,9 +31,9 @@
 
 
 #define DEVICE_NAME             "Anycubic Kobra Neo"
-#define FIRMWARE_VER            "V1.3.3-jokubasver-v1.2"
+#define FIRMWARE_VER            "V1.3.3-NuclearPhoenix-v1.0"
 #define BUILD_VOLUME            "220*220*250 (mm)"
-#define TECH_SUPPORT            "github.com/jokubasver"
+#define TECH_SUPPORT            "github.com/NuclearPhoenixx"
 
 
 
