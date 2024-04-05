4ac0c5dd9a3d18ed9c3030a32d81e8fd5374ea01
NuclearPhoenixx <nuclearphoenix@public-files.de>
2024-04-05 15:43:56 +0200

    decrease min temp for bed PID autotune

--- source/Marlin/Configuration.h
+++ source/Marlin/Configuration.h
@@ -1653,7 +1653,7 @@
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
 #define PREHEAT_1_TEMP_HOTEND 190
-#define PREHEAT_1_TEMP_BED     60
+#define PREHEAT_1_TEMP_BED     50
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
--- source/Marlin/src/lcd/menu/menu.h
+++ source/Marlin/src/lcd/menu/menu.h
@@ -31,7 +31,7 @@
 
 
 #define DEVICE_NAME             "Anycubic Kobra Neo"
-#define FIRMWARE_VER            "V1.3.3 (NP-Mod V1.3.4)"
+#define FIRMWARE_VER            "V1.3.3 (NP-Mod V1.3.5)"
 #define BUILD_VOLUME            "220 x 220 x 250 (mm)"
 #define TECH_SUPPORT            "github.com/NuclearPhoenixx"
 
